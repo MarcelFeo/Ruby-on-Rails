eafda1c31298fc2cb88d496cec1207a87bcc7659
MarcelFeo <marcelfeo29@gmail.com>
2023-06-01 22:25:45 -0300

    Estudo sobre o Model e a View

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  # faz com que o campo description seja sempre necessário
+  validates :description, presence: true
+  # faz com que o title tenha no mínimo 5 caracteres
+  validates :title, length: { minimum: 5 }
+
+  before_save do
+    self.title = self.title + ' Ok'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  # coloca o home como página inicial
+  root 'home#index'
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
